2147893755b1d4d39695621c820c92dfe1f4e3b9
13034734324 <1031622947@qq.com>
2023-01-31 16:09:39 +0800

    登录页

--- web/vue-element-admin/src/layout/components/Sidebar/Logo.vue
+++ web/vue-element-admin/src/layout/components/Sidebar/Logo.vue
@@ -2,11 +2,11 @@
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <img v-if="logo" src="@/assets/images/logo-Ico.png" class="sidebar-logo">
         <h1 v-else class="sidebar-title">{{ title }} </h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <img v-if="logo" src="@/assets/images/logo-Ico.png" class="sidebar-logo">
         <h1 class="sidebar-title">{{ title }} </h1>
       </router-link>
     </transition>
@@ -25,7 +25,7 @@ export default {
   data() {
     return {
       title: 'Caviar',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      logo: '@/assets/images/logo-Ico.png'
     }
   }
 }
--- web/vue-element-admin/src/views/login/index.vue
+++ web/vue-element-admin/src/views/login/index.vue
@@ -1,53 +1,63 @@
 <template>
-  <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+  <div style="min-width:1085px;background:linear-gradient(130deg, #0f75ff 40%, #2ddcd3  100%); overflow: hidden;height: 100%">
+    <el-row type="flex" justify="center" align="middle" style=" height:100%;">
 
-      <div class="title-container">
-        <h3 class="title">系统登录</h3>
-      </div>
+      <el-col style="width:1085px;">
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
+        <el-card class="um-loginbox">
 
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
+          <div class="um-loginleft"><span><img src="@/assets/images/logo-login.png" height="30%" width="30%"></span></div>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
-    </el-form>
+          <div class="um-loginright">
+
+            <div class="um-systemname">Caviar后台管理系统</div>
+
+            <el-row class="um-login">
+              <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+                <el-col class="um-logincol">手机号</el-col>
+                <el-form-item prop="username">
+                  <el-input
+                    ref="username"
+                    v-model="loginForm.username"
+                    name="username"
+                    type="text"
+                    tabindex="1"
+                    auto-complete="on"
+                    placeholder="请输入手机号"
+                    clearable
+                    prefix-icon="el-icon-user"
+                  />
+                </el-form-item>
+                <el-col class="um-logincol">密码</el-col>
+                <el-form-item prop="password"><el-input
+                  :key="passwordType"
+                  ref="password"
+                  v-model="loginForm.password"
+                  :type="passwordType"
+                  placeholder="请输入密码"
+                  name="password"
+                  tabindex="2"
+                  auto-complete="on"
+                  show-password
+                  prefix-icon="el-icon-lock"
+                  @keyup.enter.native="handleLogin"
+                /></el-form-item>
+
+                <el-col class="um-loginbtn"><el-button type="primary" round :loading="loading" @click.native.prevent="handleLogin">登录</el-button></el-col>
+              </el-form>
+            </el-row>
+
+          </div>
+
+        </el-card>
+
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-
+import '@/styles/uimaker.css'
 export default {
   name: 'Login',
   data() {
