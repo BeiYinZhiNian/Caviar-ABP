ae8b113a33936e61eaf7e741741adf83a7af9db5
13034734324 <1031622947@qq.com>
2023-02-17 16:58:25 +0800

    clientName

--- web/vue-element-admin/src/views/systemManagement/journal/loginLog.vue
+++ web/vue-element-admin/src/views/systemManagement/journal/loginLog.vue
@@ -35,6 +35,7 @@
         <el-table-column label="序号" type="index" width="80" align="center" />
         <el-table-column :show-overflow-tooltip="true" prop="userNameOrEmailAddress" label="登录账号" />
         <el-table-column :show-overflow-tooltip="true" prop="browserInfo" label="浏览器" />
+        <el-table-column :show-overflow-tooltip="true" prop="clientName" label="客户端" />
         <el-table-column :show-overflow-tooltip="true" prop="result" label="登录结果">
           <template slot-scope="scope">
             <el-tag v-if="scope.row.result == 1" type="success">登录成功</el-tag>
